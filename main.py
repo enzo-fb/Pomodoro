9bc39f279a318da899fda48fa0109361cb19cc04
Enzo F Borges <enzo.alternativo@hotmail.com>
2025-03-19 20:29:11 -0300

    Update main.py

--- main.py
+++ main.py
@@ -34,50 +34,53 @@ def pomodoro(tempo, descanso):
                 if seg == 0:
                     seg = 59
                     min -= 1
-
-while True:
-    print("-"*10+"Welcome Pomodoro Time"+"-"*10)   
-    print("1 - Traditional Pomodoro\n2 - Edit times\n3 - Exit")
-    escolha = input()     
-    if(escolha == "1"):
-        ciclos = 0
-        while True:
-                pomodoro(25,False)
-                ciclos += 1
-                if ciclos == 4:
-                    pomodoro(15, True)
-                    decisao = int (input("Enter 1 to restart the cycle or 2 to go back to the main menu:\n"))
-                    if decisao == 2:
-                        break
-                    print("Returning to focus...")
-                    ciclos = 0
-                else: 
-                    pomodoro(5,True)
-                    print("Returning to focus...")
-                    time.sleep(3)
-        pass
-    elif (escolha == "2"):
-        minutos_foco = int(input("Enter the focus time (in minutes):"))
-        minutos_descanso_menor = int (input("Enter the shorter break time (in minutes):\n"))
-        minutos_descanso_maior = int (input("Enter the longer break time (in minutes):\n"))
-        qntd_ciclos = int (input("Number of cycles until the long break:\n"))
-        ciclos = 0
-        while True:
-                pomodoro(minutos_foco,False)
-                ciclos += 1
-                if ciclos == qntd_ciclos:
-                    pomodoro(minutos_descanso_maior, True)
-                    decisao = int (input("Enter 1 to restart the cycle or 2 to go back to the main menu:\n"))
-                    if decisao == 2:
-                        break
-                    print("Returning to focus...")
-                    ciclos = 0
-                else: 
-                    pomodoro(minutos_descanso_menor,True)
-                    print("Returning to focus...")
-                    time.sleep(3)
-        pass
-    elif(escolha == "3"):
-        print("Exiting...")
-        exit()
-        
+def main():
+    while True:
+        print("-"*10+"Welcome Pomodoro Time"+"-"*10)   
+        print("1 - Traditional Pomodoro\n2 - Edit times\n3 - Exit")
+        escolha = input()     
+        if(escolha == "1"):
+            ciclos = 0
+            while True:
+                    pomodoro(25,False)
+                    ciclos += 1
+                    if ciclos == 4:
+                        pomodoro(15, True)
+                        decisao = int (input("Enter 1 to restart the cycle or 2 to go back to the main menu:\n"))
+                        if decisao == 2:
+                            break
+                        print("Returning to focus...")
+                        ciclos = 0
+                    else: 
+                        pomodoro(5,True)
+                        print("Returning to focus...")
+                        time.sleep(3)
+            pass
+        elif (escolha == "2"):
+            minutos_foco = int(input("Enter the focus time (in minutes):"))
+            minutos_descanso_menor = int (input("Enter the shorter break time (in minutes):\n"))
+            minutos_descanso_maior = int (input("Enter the longer break time (in minutes):\n"))
+            qntd_ciclos = int (input("Number of cycles until the long break:\n"))
+            ciclos = 0
+            while True:
+                    pomodoro(minutos_foco,False)
+                    ciclos += 1
+                    if ciclos == qntd_ciclos:
+                        pomodoro(minutos_descanso_maior, True)
+                        decisao = int (input("Enter 1 to restart the cycle or 2 to go back to the main menu:\n"))
+                        if decisao == 2:
+                            break
+                        print("Returning to focus...")
+                        ciclos = 0
+                    else: 
+                        pomodoro(minutos_descanso_menor,True)
+                        print("Returning to focus...")
+                        time.sleep(3)
+            pass
+        elif(escolha == "3"):
+            print("Exiting...")
+            exit()
+            
+if __name__ == "__main__":
+    main()
+            
